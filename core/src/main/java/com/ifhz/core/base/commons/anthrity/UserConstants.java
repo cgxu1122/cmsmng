2af8c6033f3646e046c1b2ddad57c868c4a1e509
liruibo <liruibo@puhuifinance.com.cn>
2014-06-01 03:11:12 +0800

    update user

--- core/src/main/java/com/ifhz/core/base/commons/anthrity/UserConstants.java
+++ core/src/main/java/com/ifhz/core/base/commons/anthrity/UserConstants.java
@@ -10,11 +10,15 @@ package com.ifhz.core.base.commons.anthrity;
 public class UserConstants {
 
     // 用户类型
-    public static final int USER_TYPE_NORMAL = 1;
-    public static final int USER_TYPE_MANAGER = 2;
+    public static final int USER_TYPE_NORMAL = 1; //普通用户
+    public static final int USER_TYPE_MANAGER = 2; //负责人
 
     // 用户状态
-    public static final int USER_STATUS_ENABLE = 1;
-    public static final int USER_STATUS_DISABLE = 2;
+    public static final int USER_STATUS_ENABLE = 1; //启用
+    public static final int USER_STATUS_DISABLE = 2; //禁用
 
+    //用户角色
+    public static final int USER_ROLE_NORMAL = 1;//普通用户角色
+    public static final int USER_ROLE_MANAGER = 2; //负责人角色
+    public static final int USER_ROLE_ADMIN = 3; //管理员角色
 }
