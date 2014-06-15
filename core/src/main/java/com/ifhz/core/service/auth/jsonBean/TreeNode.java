d24de8078b1e2aab692226f4fdad934af430cbff
chenggang.xu <chenggang.xu@qunar.com>
2014-06-16 00:23:13 +0800

    api接口,pkgmng提交

--- core/src/main/java/com/ifhz/core/service/auth/jsonBean/TreeNode.java
+++ core/src/main/java/com/ifhz/core/service/auth/jsonBean/TreeNode.java
@@ -68,50 +68,35 @@ public class TreeNode implements Serializable {
 
     public static void main(String[] args) throws Exception {
         TreeNode root = new TreeNode();
-        root.setId(1);
-        root.setOpen("1");
-        root.setText("root");
-        List<TreeNode> item = Lists.newArrayList();
-        List<TreeNode> item2 = Lists.newArrayList();
-        TreeNode node1 = new TreeNode();
-        node1.setId(8);
-        node1.setText("8");
-        node1.setOpen("1");
-        TreeNode node2 = new TreeNode();
-        node2.setId(7);
-        node2.setText("7");
-        node2.setOpen("1");
-        TreeNode node3 = new TreeNode();
-        node3.setId(9);
-        node3.setText("9");
-        node3.setOpen("1");
+        root.setId(0);
+        TreeNode p = new TreeNode();
+        root.setTreeNodeList(Lists.newArrayList(p));
+        p.setId(1);
+        p.setText("1-");
+        p.setOpen("1");
+        TreeNode n2 = gen(2);
+        TreeNode n3 = gen(3);
+        TreeNode n4 = gen(4);
+        TreeNode n5 = gen(5);
+        TreeNode n6 = gen(6);
+        TreeNode n7 = gen(7);
+        List<TreeNode> list = Lists.newArrayList(n2, n3, n4);
+        List<TreeNode> list2 = Lists.newArrayList(n5, n6);
+        List<TreeNode> list3 = Lists.newArrayList(n7);
+        n3.setTreeNodeList(list2);
+        n5.setTreeNodeList(list3);
+        p.setTreeNodeList(list);
 
-        TreeNode node4 = new TreeNode();
-        node4.setId(5);
-        node4.setText("5");
-        node4.setOpen("1");
-        TreeNode node5 = new TreeNode();
-        node5.setId(6);
-        node5.setText("6");
-        node5.setOpen("1");
-        TreeNode node6 = new TreeNode();
-        node6.setId(10);
-        node6.setText("10");
-        node6.setOpen("1");
-        node6.setChecked("1");
+        System.out.println(JSON.toJSONString(root));
 
-        item.add(node1);
-        item.add(node2);
-        item2.add(node4);
-        item2.add(node5);
-        item2.add(node6);
-        node3.setTreeNodeList(item2);
-        item.add(node3);
+    }
 
-        root.setTreeNodeList(item);
+    private static TreeNode gen(int id) {
+        TreeNode node = new TreeNode();
+        node.setId(id);
+        node.setText(id + "-");
+        node.setOpen("1");
 
-
-        String json = JSON.toJSONString(root);
-        System.out.println(json);
+        return node;
     }
 }
