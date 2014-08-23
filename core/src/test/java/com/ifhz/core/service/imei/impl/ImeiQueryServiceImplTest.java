c508f520850f9d10a4b9cce25e5ffab5bd1b07bc
chenggang.xu <chenggang.xu@qunar.com>
2014-08-23 23:22:16 +0800

    新权限授权

--- core/src/test/java/com/ifhz/core/service/imei/impl/ImeiQueryServiceImplTest.java
+++ core/src/test/java/com/ifhz/core/service/imei/impl/ImeiQueryServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.ifhz.core.service.imei.impl;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.ifhz.core.adapter.ImeiQueryAdapter;
 import com.ifhz.core.service.imei.ImeiQueryService;
 import com.ifhz.core.service.imei.bean.DataLogResult;
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Set;
 
 public class ImeiQueryServiceImplTest extends BaseTest {
 
@@ -20,8 +21,8 @@ public class ImeiQueryServiceImplTest extends BaseTest {
     @Test
     public void testQueryListByImeiList() throws Exception {
 
-        List<String> imeiList = Lists.newArrayList("864730013656894",
-                "864730013658239",
+        Set<String> imeiList = Sets.newHashSet("863925024510590",
+                "352956061737216",
                 "864730013660128",
                 "864730013534216",
                 "864730013665135",
